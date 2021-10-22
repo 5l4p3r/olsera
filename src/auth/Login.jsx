84ca677d282432dabe8c0f7cc3f7812e272608b0
5l4p3r <estehtawar5000@gmail.com>
2021-10-23 05:34:13 +0700

    alert

--- src/auth/Login.jsx
+++ src/auth/Login.jsx
@@ -1,4 +1,4 @@
-import { Button, Card, TextField, Typography } from '@mui/material'
+import { Button, Card, TextField, Typography, Container, Modal } from '@mui/material'
 import { Box } from '@mui/system'
 import axios from 'axios'
 import Cookies from 'js-cookie'
@@ -10,6 +10,7 @@ const Login = () => {
     const {auth,setUserid,setAuth} = useContext(AllContext)
     const [user, setUser] = useState('')
     const [email, setEmail] = useState('')
+    const [open, setOpen] = useState(false)
     const history = useHistory()
 
     if(auth){
@@ -18,47 +19,64 @@ const Login = () => {
         )
     }else{
         return (
-            <Box sx={{p:6, display: 'flex', justifyContent:'center'}}>
-                <Card variant="outlined">
-                    <Box sx={{ p: 1, bgcolor: 'grey.500', alignItems:'center'}}>
-                        <Box sx={{p:1}}>
-                            <Typography variant="h5">
-                                {"Login"}
-                            </Typography>
-                        </Box>
-                        <Box sx={{p:1}}>
-                            <TextField label="Userid" variant="outlined" onChange={(e)=>{setUser(e.target.value)}}/>
-                        </Box>
-                        <Box sx={{p:1}}>
-                            <TextField label="Email" variant="outlined" onChange={(e)=>{setEmail(e.target.value)}}/>
-                        </Box>
-                        <Box sx={{ p:1,alignItems:'flex-end' }}>
-                            <Button color="inherit" variant="contained" onClick={()=>{
-                                history.push('/')
-                            }}>Cancel</Button> &nbsp;
-                            <Button color="primary" variant="contained" onClick={async()=>{
-                                try {
-                                    await axios.get(`https://jsonplaceholder.typicode.com/users/${user}`).then(async(res)=>{
-                                        if(email === res.data.email){
-                                            await setAuth(true)
-                                            await setUserid(res.data.id)
+            <Container fixed>
+                <Box sx={{p:6, display: 'flex', justifyContent:'center'}}>
+                    <Card variant="outlined">
+                        <Box sx={{ p: 1, bgcolor: 'grey.500', alignItems:'center'}}>
+                            <Box sx={{p:1}}>
+                                <Typography variant="h5">
+                                    {"Login"}
+                                </Typography>
+                            </Box>
+                            <Box sx={{p:1}}>
+                                <TextField label="Userid" variant="outlined" onChange={(e)=>{setUser(e.target.value)}}/>
+                            </Box>
+                            <Box sx={{p:1}}>
+                                <TextField label="Email" variant="outlined" onChange={(e)=>{setEmail(e.target.value)}}/>
+                            </Box>
+                            <Box sx={{ p:1,alignItems:'flex-end' }}>
+                                <Button color="inherit" variant="contained" onClick={()=>{
+                                    history.push('/')
+                                }}>Cancel</Button> &nbsp;
+                                <Button color="primary" variant="contained" onClick={async()=>{
+                                    try {
+                                        await axios.get(`https://jsonplaceholder.typicode.com/users/${user}`).then(async(res)=>{
+                                            setAuth(true)
+                                            setUserid(res.data.id)
                                             await Cookies.set('auth',true)
                                             await Cookies.set('userid', res.data.id)
                                             await Cookies.set('email', email)
-                                        }else{
-                                            return (
-                                                alert('UserId & Email Do Not match')
-                                            )
-                                        }
-                                    })
-                                } catch (error) {
-                                    console.log(error);
-                                }
-                            }}>Login</Button>
+                                        })
+                                    } catch (error) {
+                                        setOpen(true)
+                                    }
+                                }}>Login</Button>
+                            </Box>
                         </Box>
-                    </Box>
-                </Card>
-            </Box>
+                    </Card>
+                </Box>
+                <Modal 
+                    open={open}
+                    onClose={()=>{
+                        setOpen(false)
+                    }}>
+                        <Box
+                            sx={{
+                            position: 'absolute',
+                            top: '50%',
+                            left: '50%',
+                            transform: 'translate(-50%, -50%)',
+                            maxWidth:'95%',
+                            bgcolor: '#fff',
+                            borderRadius:5,
+                            p: 1,
+                        }}>
+                            <Box>
+                                <Typography variant="h6" color="initial">User id and Email not match !</Typography>
+                            </Box>
+                        </Box>
+                </Modal>
+            </Container>
         )
     }
 }
